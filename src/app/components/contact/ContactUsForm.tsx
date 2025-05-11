eb598f75145f7a2b9bff2224326ae57349363813
eedreese <idbalorg@gmail.com>
2025-05-11 20:55:35 +0100

    feat: style contact page

--- src/app/components/contact/ContactUsForm.tsx
+++ src/app/components/contact/ContactUsForm.tsx
@@ -24,68 +24,86 @@ const ContactUsForm = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form Submitted:", formData);
-    // Add your submission logic here
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h4>
+    <form
+      className="md:p-8 md:bg-[#F5F5F5F5] md:rounded-4xl mb-[107px]"
+      onSubmit={handleSubmit}
+    >
+      <h4 className="hidden md:block md:font-semibold md:text-[16px] mb-8">
         Have a question for us? Let us know & we will respond within 24 hours
       </h4>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-row justify-between">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={formData.firstName}
+            onChange={handleChange}
+            className="h-10 md:h-14 md:p-4 bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={formData.lastName}
+            onChange={handleChange}
+            className="h-10 md:h-14 md:p-4 bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
+          />
+        </div>
 
-      <div>
         <input
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          value={formData.firstName}
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
           onChange={handleChange}
+          className="h-10 md:h-14 md:p-4 bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
         />
+
         <input
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          value={formData.lastName}
+          type="tel"
+          name="phone"
+          placeholder="Phone Number"
+          value={formData.phone}
           onChange={handleChange}
+          className="h-10 md:h-14 md:p-4 bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
+        />
+
+        <select
+          className="h-10 md:h-14 md:p-4 bg-[#EEEEEE] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
+          name="reason"
+          value={formData.reason}
+          onChange={handleChange}
+        >
+          <option value="">Reason For Inquiry</option>
+          <option value="Support">Support</option>
+          <option value="Sales">Sales</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <textarea
+          name="message"
+          placeholder="Message"
+          value={formData.message}
+          onChange={handleChange}
+          className="h-2 md:h-20 md:p-4 bg-[#FFFFFF] border-[1px] rounded-[10px] border-[#CCCCCCCC]"
         />
       </div>
 
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
-      />
-
-      <input
-        type="tel"
-        name="phone"
-        placeholder="Phone Number"
-        value={formData.phone}
-        onChange={handleChange}
-      />
-
-      <select name="reason" value={formData.reason} onChange={handleChange}>
-        <option value="">Reason For Inquiry</option>
-        <option value="Support">Support</option>
-        <option value="Sales">Sales</option>
-        <option value="Other">Other</option>
-      </select>
-
-      <textarea
-        name="message"
-        placeholder="Message"
-        value={formData.message}
-        onChange={handleChange}
-      />
-
-      {/* Replace with your reCAPTCHA component if needed */}
-      <div>
+      {/* Replace with Captcha */}
+      <div className="mt-7">
         <input type="checkbox" required /> I'm not a robot
       </div>
 
-      <button type="submit">Send Message</button>
+      <button
+        className="w-[100%] h-[67px] rounded-4xl bg-[#FD4A09] font-medium text-[20px] text-[#FFFFFF] mt-14 "
+        type="submit"
+      >
+        Send Message
+      </button>
     </form>
   );
 };
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -21,27 +21,41 @@ const contactDetails = [
 const Contact = () => {
   return (
     <div className="px-[22px]">
-      <h3>Support Center</h3>
-      <ul>
+      <h3 className="text-center font-bold text-[20px] md:text-[45px] leading-6 md:mt-[40px]">
+        Support Center
+      </h3>
+      <ul className=" flex flex-col justify-start md:justify-center md:flex-row md:gap-[80px] gap-[50px] mt-[45px] md:mt-[80px] mb-[50px] md:mb-[120px] ">
         {navLinks.map((link) => (
-          <li key={link.name}>
-            <Link href={link.href}>{link.name}</Link>
+          <li className="  " key={link.name}>
+            <Link
+              className="font-semibold text-[12px] md:text-[20px] text-[#222222B2] md:text-[#222222] leading-[100%] md:leading-[24px] underline underline-offset-4 "
+              href={link.href}
+            >
+              {link.name}
+            </Link>
           </li>
         ))}
       </ul>
 
-      <div>
+      <div className="flex flex-col md:flex-row ">
         <div>
-          <h4>Contact Us</h4>
-          <p>
+          <h4 className="mb-[6px] md:mb-[18px] text-[16px] md:text-[26px] font-semibold leading-6 ">
+            Contact Us
+          </h4>
+          <p className="text-[12px] md:text-[18px] text-[#222222E5] leading-[18px] font-normal md:font-medium mb-[20px] md:mb-[76px]">
             For any urgent inquiries, please contact us using the details
             provided below.
           </p>
-          <ul>
+          <ul className=" flex flex-col gap-7 md:gap-10 ">
             {contactDetails.map((detail) => (
-              <li key={detail.content}>
+              <li
+                className=" flex flex-row gap-3 md:gap-8 "
+                key={detail.content}
+              >
                 <Image src={detail.icon} alt={detail.content} />
-                <span>{detail.content}</span>
+                <span className="font-normal md:text-[18px] text-[12px] leading-6  ">
+                  {detail.content}
+                </span>
               </li>
             ))}
           </ul>
